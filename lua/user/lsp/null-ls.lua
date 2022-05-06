8d01437058968f5ba3203711e302f99a43b07f4b
Mauricio Natanael Ferreira <contato@ferreiramauricio.com>
2022-05-06 13:32:17 -0300

    TODO: document prettier setup

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,9 +11,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.prettier
     -- diagnostics.flake8
 	},
 })
